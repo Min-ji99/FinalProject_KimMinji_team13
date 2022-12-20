dc9536d0fe6d73348385d810aa02fb3f96dc4755
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-20 11:52:48 +0900

    Update : Swaager ApiInfo 추가

--- src/main/java/com/likelion/sns/configuration/SwaggerConfiguration.java
+++ src/main/java/com/likelion/sns/configuration/SwaggerConfiguration.java
@@ -2,8 +2,10 @@ package com.likelion.sns.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,9 +16,16 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.jpa.exercise.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.likelion.sns.controller"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("멋쟁이사자처럼 project")
+                .description("")
                 .build();
     }
 }
