25b70310a5e85c67af80e43bd5f375f9d55e7f34
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-26 23:31:34 +0900

    Update : createdAt, lastModifiedAt 조회 시 포맷 변경하도록 수정
    yyyy-mm-ddThh:mm:ss.a
    --> yyyy/mm/dd hh:mm:ss로 변경

--- src/main/java/com/likelion/sns/domain/dto/PostDto.java
+++ src/main/java/com/likelion/sns/domain/dto/PostDto.java
@@ -1,12 +1,15 @@
 package com.likelion.sns.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.likelion.sns.domain.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.domain.Page;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -18,7 +21,9 @@ public class PostDto {
     private String title;
     private String body;
     private String userName;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/dd/mm hh:mm:ss")
     private LocalDateTime createdAt;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/dd/mm hh:mm:ss")
     private LocalDateTime lastModifiedAt;
 
     public static Page<PostDto> toList(Page<Post> postEntities) {
