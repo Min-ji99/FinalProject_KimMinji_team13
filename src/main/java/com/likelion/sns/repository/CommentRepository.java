487add758546dc179bd0cad60a9fd88035fd9e68
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-01-05 15:33:43 +0900

    Update : 포스트, 댓글, 좋아요 delete를 soft delete로 수정
    1. Post 삭제 시 해당 포스트와 연관된 Comment, like도 같이 삭제
    2. Post, comment에서 delete 대신 update deleted_at을 현재 시간으로
    3. 조회시 deleted_at이 Null 인 경우만 조회

--- src/main/java/com/likelion/sns/repository/CommentRepository.java
+++ src/main/java/com/likelion/sns/repository/CommentRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByPost(Pageable pageable, Post post);
+    void deleteAllByPost(Post post);
 }
