3fa0e0e1fced67d43d7c8f5d62e9789824b2b527
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-01-09 15:48:26 +0900

    Feat : Swagger ApiOperation 추가
    Controller의 메서드 설명 추가

--- src/main/java/com/likelion/sns/controller/CommentRestController.java
+++ src/main/java/com/likelion/sns/controller/CommentRestController.java
@@ -6,6 +6,8 @@ import com.likelion.sns.domain.dto.comment.CommentDto;
 import com.likelion.sns.domain.dto.comment.CommentModifyRequest;
 import com.likelion.sns.domain.dto.comment.CommentWriteRequest;
 import com.likelion.sns.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +21,7 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 @RequestMapping("/api/v1/posts")
 @Slf4j
+@Api(tags="Comment")
 public class CommentRestController {
     private final CommentService commentService;
 
@@ -26,21 +29,25 @@ public class CommentRestController {
         this.commentService = commentService;
     }
 
+    @ApiOperation(value="댓글 등록", notes="로그인 후 존재하는 포스트의 댓글 등록")
     @PostMapping("/{postId}/comments")
     public Response<CommentDto> writeComment(@PathVariable Integer postId, @ApiIgnore Authentication authentication, @RequestBody CommentWriteRequest dto){
         CommentDto commentDto =commentService.writeComment(postId, dto, authentication.getName());
         return Response.success(commentDto);
     }
+    @ApiOperation(value="댓글 수정", notes="로그인 후 사용자 작성한 댓글만 수정 가능")
     @PutMapping("/{postId}/comments/{id}")
     public Response<CommentDto> modifyComment(@PathVariable Integer id, @ApiIgnore Authentication authentication, @RequestBody CommentModifyRequest dto){
         CommentDto commentDto =commentService.modifyComment(id, dto, authentication.getName());
         return Response.success(commentDto);
     }
+    @ApiOperation(value="댓글 삭제", notes="로그인 후 사용자가 작성한 댓글만 삭제 가능")
     @DeleteMapping("/{postId}/comments/{id}")
     public Response<CommentDeleteResponse> deleteComment(@PathVariable Integer id, @ApiIgnore Authentication authentication){
         CommentDeleteResponse commentDeleteResponse =commentService.deleteComment(id, authentication.getName());
         return Response.success(commentDeleteResponse);
     }
+    @ApiOperation(value="댓글 리스트 조회", notes="해당 포스트에 달린 댓글 리스트 조회")
     @GetMapping("/{postId}/comments")
     public Response<Page<CommentDto>> getCommentList(@PageableDefault(size=10)
                                                      @SortDefault(sort="createdAt", direction= Sort.Direction.DESC) Pageable pageable, @PathVariable Integer postId){
--- src/main/java/com/likelion/sns/controller/PostRestController.java
+++ src/main/java/com/likelion/sns/controller/PostRestController.java
@@ -1,15 +1,13 @@
 package com.likelion.sns.controller;
 
 import com.likelion.sns.domain.dto.*;
-import com.likelion.sns.domain.dto.comment.CommentDeleteResponse;
-import com.likelion.sns.domain.dto.comment.CommentDto;
-import com.likelion.sns.domain.dto.comment.CommentModifyRequest;
-import com.likelion.sns.domain.dto.comment.CommentWriteRequest;
 import com.likelion.sns.domain.dto.post.PostDto;
 import com.likelion.sns.domain.dto.post.PostModifyRequet;
 import com.likelion.sns.domain.dto.post.PostResponse;
 import com.likelion.sns.domain.dto.post.PostWriteRequest;
 import com.likelion.sns.service.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +21,7 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 @RequestMapping("/api/v1/posts")
 @Slf4j
+@Api(tags="Post")
 public class PostRestController {
     private final PostService postService;
 
@@ -30,33 +29,39 @@ public class PostRestController {
         this.postService = postService;
     }
 
+    @ApiOperation(value="포스트 등록", notes="로그인 후 포스트 등록 가능")
     @PostMapping
     public Response<PostResponse> write(@RequestBody PostWriteRequest dto, @ApiIgnore Authentication authentication){
         log.info("controller : {}", authentication);
         PostResponse postResponse =postService.writePost(dto, authentication.getName());
         return Response.success(postResponse);
     }
+    @ApiOperation(value="포스트 리스트 조회", notes="포스트 최신순으로 20개씩 조회 가능")
     @GetMapping
     public Response<Page<PostDto>> getPostlist(@PageableDefault(size=20)
                                             @SortDefault(sort="createdAt", direction=Sort.Direction.DESC) Pageable pageable){
         Page<PostDto> posts=postService.getPostlist(pageable);
         return Response.success(posts);
     }
+    @ApiOperation(value="포스트 상세 조회")
     @GetMapping("/{postId}")
     public Response<PostDto> findPostById(@PathVariable Integer postId){
         PostDto post=postService.findPostById(postId);
         return Response.success(post);
     }
+    @ApiOperation(value="포스트 수정", notes = "로그인 후 사용자가 작성한 포스트만 수정 가능")
     @PutMapping("/{id}")
     public Response<PostResponse> modify(@PathVariable Integer id, @RequestBody PostModifyRequet dto, @ApiIgnore Authentication authentication){
         PostResponse postResponse =postService.modifyPost(id, dto, authentication.getName());
         return Response.success(postResponse);
     }
+    @ApiOperation(value="포스트 삭제", notes="로그인 후 사용자가 작성한 포스트만 삭제 가능")
     @DeleteMapping("/{id}")
     public Response<PostResponse> delete(@PathVariable Integer id, @ApiIgnore Authentication authentication){
         PostResponse postResponse=postService.deletePost(id, authentication.getName());
         return Response.success(postResponse);
     }
+    @ApiOperation(value="마이 피드", notes="로그인한 사용자가 작성한 포스트만 확인 가능")
     @GetMapping("/my")
     public Response<Page<PostDto>> myFeed(@PageableDefault(size=20)
                                               @SortDefault(sort="createdAt", direction=Sort.Direction.DESC) Pageable pageable,
